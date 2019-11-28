fa10fc0fabef4a8d7f0ce3d9ae1494a57ae45ec7
rivu <58026024+unphydra@users.noreply.github.com>
2019-11-28 10:01:59 +0530

    perfprmSave is modified
    What changes you have made?
    -data structure is changed
    -perform save is modified
    
    Why this change is necessary?
    - i need to change the data structure to make it easy
    -so i need to change my performSave which saveing my records
    
    What is the impact of the change?
    - now i can save my data in my record

--- src/performSaveLib.js
+++ src/performSaveLib.js
@@ -1,17 +1,7 @@
 const performSave = function(records, args, date) {
   let { empId, beverage, qty } = args;
-  let transDetails = { beverage, qty, date };
-  if (records.hasOwnProperty(empId)) {
-    records[empId].transaction.push(transDetails);
-    let prevQty = records[empId].totalQty;
-    records[empId].totalQty = prevQty + qty;
-    let savedTrans = { empId, beverage, qty, date };
-    return [savedTrans];
-  }
-  records[empId] = {};
-  records[empId].transaction = transDetails;
-  records[empId].totalQty = qty;
-  savedTrans = { empId, beverage, qty, date };
+  let savedTrans = { empId, beverage, qty, date };
+  records.push(savedTrans);
   return [savedTrans];
 };
 
